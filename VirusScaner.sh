6c2827f5d02393868fe5c5f0b45638fc60a089e4
ian <iancha.alu@iesbenigaslo.es>
2022-04-08 12:16:05 +0200

    Casi acabado

---
+++ VirusScaner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
+DIRTOSCAN=".";
+
+for S in ${DIRTOSCAN}; do
+ DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
+
+ echo "Starting a daily scan of "$S" directory.
+ Amount of data to be scanned is "$DIRSIZE".";
+
+ clamscan -ri "$S" >> "$LOGFILE";
+
+ # get the value of "Infected lines"
+ MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
+
+ # if the value is not equal to zero, send an email with the log file attached
+ if [ "$MALWARE" -ne "0" ];then
+ # using heirloom-mailx below
+ echo "Malware Found";
+ fi 
+done
+
+exit 0 
